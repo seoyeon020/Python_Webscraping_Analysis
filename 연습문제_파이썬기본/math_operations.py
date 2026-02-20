e6540e80b01614380bbf54b318e9c0a0a15a46cd
seoyeon020 <ksheee7000@naver.com>
2026-02-21 00:21:20 +0900

    8 update

---
+++ 연습문제_파이썬기본/math_operations.py
@@ -0,0 +1,17 @@
+import math
+
+# 원의 넓이
+def get_circle_area(radius):
+    return math.pi * (radius ** 2)
+
+# 직사각형 넓이
+def get_rectangle_area(width, height):
+    return width * height
+
+# 팩토리얼
+def get_factorial(n):
+    return math.factorial(n)
+
+# 최대공약수
+def get_gcd(a, b):
+    return math.gcd(a, b)
